ed017341711a87dd850a00549abf5a2f6d1cf1fb
Will Booker <william@Williams-MacBook-Pro.local>
2017-03-03 01:15:25 -0600

    Working on Fixing the Net

--- MarchMadness2017/src/SimpleNet.java
+++ MarchMadness2017/src/SimpleNet.java
@@ -8,15 +8,18 @@ public class SimpleNet {
 
 	//A static queue used when cascading
 	private static Queue<SimpleNet> bfsqueue = new LinkedList<SimpleNet>();
-	
+
 	//A list of all the nodes that have been created
 	public static ArrayList<SimpleNet> allnodes = new ArrayList<SimpleNet>();
 	public static ArrayList<SimpleNet> startnodes = new ArrayList<SimpleNet>(); 
+	
+	private static int ID;
+	public int id;
 
 	//The nodes weights and output nodes
 	public ArrayList<Double> weights;
 	public ArrayList<SimpleNet> children;
-	
+
 	private final double offset;
 
 	//The value of the current node
@@ -24,6 +27,7 @@ public class SimpleNet {
 
 	//Creates a new blank SimpleNet node
 	public SimpleNet(boolean startnode, double offset) {
+		this.id = ID++;
 		this.offset = offset;
 		this.weights = new ArrayList<Double>();
 		this.children = new ArrayList<SimpleNet>();
@@ -49,7 +53,7 @@ public class SimpleNet {
 		this.value = value;
 		bfsqueue.add(this);
 	}	
-	
+
 	//Primes the whole net
 	public static void prime(ArrayList<Double> values) {
 		for(int i=0; i<startnodes.size(); i++) {
@@ -72,39 +76,42 @@ public class SimpleNet {
 
 			//If the node has already been hit, ignore it
 			if(!hit.contains(node)) {
-				
+
 				//Add the current node to the list of hit nodes
 				hit.add(node);
+
+				System.out.println(node.id + ": " + node.value);
+
 				
 				//If the node doesn't have any children, it is an answer node
-				if(node.children.isEmpty()) {
-					answers.add(node.value);}
+				if(node.children.isEmpty()) {answers.add(node.value);}
+				
+				//If it's not an end node or a start node, get the sigma value
+				else if(!startnodes.contains(node)) {node.value = node.calcSigmoid(node.value);}
+				
+				System.out.println(node.id + ": " + node.value);
 
 				//For every child of the node...
 				for(int i=0; i<node.children.size(); i++) {
-					
+
 					//Getting the child node and weight
 					SimpleNet nextnode = node.children.get(i);
 					double nextweight = node.weights.get(i);
 
-					//If the child is in the middle of the net, run a sigmoid function
-					if(!nextnode.children.isEmpty()) {
-						nextnode.value = nextnode.calcSigmoid(nextnode.value);}
-					
 					//Add the next node to the queue
 					nextnode.value += node.value * nextweight;
 					bfsqueue.add(nextnode);
 				}
-				
+
 				//Cleaning the node value
 				node.value = 0.0;
 			}
 		}
-		
+
 		//Return the list of valid answers
 		return answers; 
 	}
-	
+
 	//Gets the list of all weights
 	public static ArrayList<Double> getWeights() {
 		ArrayList<Double> weights = new ArrayList<Double>();
@@ -113,7 +120,7 @@ public class SimpleNet {
 				weights.add(weight);}}
 		return weights;
 	}
-	
+
 	//Sets all the weights
 	public static void setWeights(ArrayList<Double> weights) {
 		int counter = 0; 
@@ -124,8 +131,9 @@ public class SimpleNet {
 
 	//Returns the value of the sigmoid function
 	private double calcSigmoid(double value) {
-		//return value;
-		return 1.0 / (1.0 + Math.exp(-(value+offset))); 
+		value = value + offset;
+		double rawvalue = 1.0 / (1.0 + Math.exp(-value));
+		return rawvalue;
 	}
 
 }
